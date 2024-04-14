82fa4ef9332941432d44af75ef5b59ce212c6c48
sangyeongseo <screeningseo@gmail.com>
2024-04-15 00:21:12 +0900

    Skeleton 이동, 점프, 방향전환

--- Assets/Script/Skeleton.cs
+++ Assets/Script/Skeleton.cs
@@ -1,39 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Skeleton : Monster
 {
-    public Transform[] wallCheck;
+    float moveDir;
 
     private void Awake()
     {
         base.Awake();
         moveSpeed = 2f;
-        jumpPower = 15f;
+        jumpPower = 5f;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
+
         if (!isHit)
         {
-            rb.velocity = new Vector2(-transform.localScale.x * moveSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(transform.localScale.x * moveSpeed, rb.velocity.y);
 
-            if (!Physics2D.OverlapCircle(wallCheck[0].position, 0.01f, layerMask) &&
-                Physics2D.OverlapCircle(wallCheck[1].position, 0.01f, layerMask) &&
-                 !Physics2D.Raycast(transform.position, -transform.localScale.x * transform.right, 1f, layerMask))
-            {
+            if (MonsterDirRight == true)
+                moveDir = -0.5f;
+            else
+                moveDir = 0.5f;
+
+            
+
+            Vector2 currentPos = transform.position; // 현재 위치 기준
+            Vector2 frontVec = new Vector2(currentPos.x + transform.localScale.x, currentPos.y); // 앞 방향
+            Vector2 topVec = new Vector2(currentPos.x + transform.localScale.x, currentPos.y + 0.7f); // 위 방향
+
+
+
+            Debug.DrawRay(frontVec, MonsterDirRight ? Vector3.right : Vector3.left, new Color(0, 1, 0));
+            Debug.DrawRay(topVec, MonsterDirRight ? Vector3.right : Vector3.left, new Color(0, 1, 0));
+
+            RaycastHit2D front = Physics2D.Raycast(frontVec, MonsterDirRight ? Vector3.right : Vector3.left, 1, LayerMask.GetMask("Platform"));
+            RaycastHit2D top = Physics2D.Raycast(topVec, MonsterDirRight ? Vector3.right : Vector3.left, 1, LayerMask.GetMask("Platform"));
+
+            if (front.collider != null) // 점프
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-            }
-            else if (Physics2D.OverlapCircle(wallCheck[1].position, 0.01f, layerMask))
-            {
-                MonsterFlip();
-            }
-        }
 
+            if (top.collider != null) // 벽 방향전환
+                MonsterFlip();
+
+
+            GroundCheck();
+            if (top.collider == null && front.collider == null && isGround)
+            {
+                Vector2 downVec = new Vector2(transform.position.x + moveDir, transform.position.y);
+                Debug.DrawRay(downVec, Vector3.down, new Color(0, 1, 0));
+                RaycastHit2D down = Physics2D.Raycast(downVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+                if (down.collider == null)
+                    MonsterFlip();
+            }
+
+        }
     }
-    protected void OnTriggerEnter2D(Collider2D collision)
+
+
+    protected void OnTriggerEnter2D(Collider2D collision) // 플레이어와 부딪히면 방향 전환
     {
         base.OnTriggerEnter2D(collision);
         if (collision.transform.CompareTag("PlayerHitBox"))
@@ -41,4 +71,6 @@ public class Skeleton : Monster
             MonsterFlip();
         }
     }
+
+
 }
