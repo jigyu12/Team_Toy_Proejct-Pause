60d5dbf57bfa038bff9fe3cfbab0f5032bc74dec
sangyeongseo <screeningseo@gmail.com>
2024-06-08 17:13:27 +0900

    Bullet 유지 조건 수정

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -105,8 +105,20 @@ public class GameManager : MonoBehaviour
                 sceneNumberNext = 2;
                 playerDamage = 1;
 
-                int savedBulletIndex = PlayerPrefs.GetInt("PlayerBulletIndex", 0);
-                player.GetComponent<PlayerMove>().ChangeBullet(savedBulletIndex);
+                if (scene.buildIndex == 1)
+                {
+                    int defaultBulletIndex = 0;
+                    PlayerPrefs.SetInt("PlayerBulletIndex", defaultBulletIndex);
+                    PlayerPrefs.Save();
+                    player.GetComponent<PlayerMove>().ChangeBullet(defaultBulletIndex);
+                }
+                else
+                {
+                    int savedBulletIndex = PlayerPrefs.GetInt("PlayerBulletIndex", 0);
+                    player.GetComponent<PlayerMove>().ChangeBullet(savedBulletIndex);
+                }
+
+
 
                 GameObject uiH5 = GameObject.Find("ui").transform.Find("h5").gameObject;
                 GameObject uiH5_Empty = GameObject.Find("ui").transform.Find("h5_empty").gameObject;
